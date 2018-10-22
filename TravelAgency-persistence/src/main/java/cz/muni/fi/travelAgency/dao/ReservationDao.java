8667b9f82c7f974296c51343cea4e44789e56c10
lyrithy <lyrithyit@gmail.com>
2018-10-22 22:08:09 +0200

    Reservation Entity, DAO, and CRUD implement

---
+++ TravelAgency-persistence/src/main/java/cz/muni/fi/travelAgency/dao/ReservationDao.java
@@ -0,0 +1,31 @@
+package cz.muni.fi.travelAgency.dao;
+import cz.muni.fi.travelAgency.entities.Customer;
+import cz.muni.fi.travelAgency.entities.Excursion;
+import cz.muni.fi.travelAgency.entities.Reservation;
+import cz.muni.fi.travelAgency.entities.Trip;
+
+import java.util.Date;
+import java.util.List;
+
+public interface ReservationDao {
+
+    Reservation create(Reservation reservation);
+
+    List<Reservation> findAll();
+
+    Reservation findById(Long id);
+
+    List<Reservation> findByCustomer(Customer customer);
+
+    List<Reservation> findByTrip(Trip trip);
+
+    void update(Reservation reservation);
+
+    void remove(Reservation reservation)  throws IllegalArgumentException;
+
+    List<Reservation> findReservationBetween(Date startDate, Date endDate);
+
+
+}
+
+
