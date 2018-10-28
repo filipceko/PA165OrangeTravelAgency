1b7fa7842a61fe8d0879c8a2a36af991df4e1094
Simona Raučinová <445616@mail.muni.cz>
2018-10-28 17:07:20 +0100

    Improved excursion tests

--- TravelAgency-persistence/src/main/java/cz/muni/fi/travelAgency/dao/ExcursionDao.java
+++ TravelAgency-persistence/src/main/java/cz/muni/fi/travelAgency/dao/ExcursionDao.java
@@ -13,56 +13,40 @@ import java.util.Collection;
 public interface ExcursionDao {
 
     /**
-     * Stores excursion into the DB
+     * Stores excursion into the DB.
      * @param excursion to be stored
-     * @throws IllegalArgumentException if input is NULL
-     * @throws javax.validation.ConstraintViolationException if parameter violates constraints
      */
     void create(Excursion excursion);
 
     /**
-     * Retrieves excursion with given ID
+     * Retrieves excursion with given ID.
      * @param id of the excursion
-     * @return excursion with given ID or NULL if none found
-     * @throws IllegalArgumentException if id is NULL
+     * @return excursion with given ID
      */
     Excursion findById(Long id);
 
     /**
-     * Retrieves all excursions
+     * Retrieves all excursions.
      * @return collection of all excursion
      */
     Collection<Excursion> findAll();
 
     /**
-     * Finds list of excursion with given destination
-     * @param destination of excursion
-     * @return collection of excursions with given destination
-     * @throws IllegalArgumentException if destination is NULL
-     */
-    Collection<Excursion> findByDestination(String destination);
-
-    /**
      * Retrieves all excursions by Trip.
      * @param trip of the excursion
-     * @return collection of all excursions by Trip
-     * @throws IllegalArgumentException if parameter is NULL or not saved yet
+     * @return collection of all excursion by Trip
      */
     Collection<Excursion> findByTrip(Trip trip);
 
     /**
      * Updates given excursion's data in the DB
      * @param excursion to be updated
-     * @throws IllegalArgumentException if parameter is NULL or not saved yet
-     * @throws org.springframework.transaction.TransactionSystemException if constraints are violated
      */
     void update(Excursion excursion);
 
     /**
-     * Erases data for given excursion form the DB
+     * Erases data for given excursion form the DB.
      * @param excursion to be erased
-     * @throws IllegalArgumentException when NULL as parameter
-     * @throws javax.validation.ConstraintViolationException when parameter is not valid
      */
     void remove(Excursion excursion) throws IllegalArgumentException;
 
