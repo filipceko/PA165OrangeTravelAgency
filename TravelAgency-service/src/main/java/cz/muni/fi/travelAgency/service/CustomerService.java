e445eed5404986f8b5eaa011e5f7b0da5819f9cb
Simona Raučinová <445616@mail.muni.cz>
2018-11-14 12:28:10 +0100

    Service layer initialization

---
+++ TravelAgency-service/src/main/java/cz/muni/fi/travelAgency/service/CustomerService.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.travelAgency.service;
+
+import cz.muni.fi.travelAgency.entities.Customer;
+
+/**
+ * An interface that defines a service access to the {@link Customer} entity.
+ *
+ * @author Simona Raucinova
+ */
+
+public interface CustomerService {
+}
