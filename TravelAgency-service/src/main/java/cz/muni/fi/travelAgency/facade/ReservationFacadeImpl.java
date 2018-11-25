f3eeeea408f75245be56d803f2b869b0b509ecb1
Filip Čekovský <filipceko@gmail.com>
2018-11-25 19:25:01 +0100

    Exception handling added

--- TravelAgency-service/src/main/java/cz/muni/fi/travelAgency/facade/ReservationFacadeImpl.java
+++ TravelAgency-service/src/main/java/cz/muni/fi/travelAgency/facade/ReservationFacadeImpl.java
@@ -2,6 +2,7 @@ package cz.muni.fi.travelAgency.facade;
 
 import cz.muni.fi.travelAgency.DTO.ReservationDTO;
 import cz.muni.fi.travelAgency.entities.Reservation;
+import cz.muni.fi.travelAgency.exceptions.DataAccessException;
 import cz.muni.fi.travelAgency.service.BeanMappingService;
 import cz.muni.fi.travelAgency.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,23 +44,46 @@ public class ReservationFacadeImpl implements ReservationFacade {
 
     @Override
     public Collection<ReservationDTO> getAllReservations() {
-        return mappingService.mapTo(service.findAll(), ReservationDTO.class);
+        Collection<Reservation> result;
+        try {
+            result = service.findAll();
+        } catch (Exception e) {
+            throw new DataAccessException("Could not obtain Reservations from the persistence layer", e);
+        }
+        return mappingService.mapTo(result, ReservationDTO.class);
     }
 
     @Override
     public ReservationDTO getById(Long id) {
-        Reservation queryResult = service.findById(id);
+        Reservation queryResult;
+        try {
+            queryResult = service.findById(id);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not obtain Reservation from the persistence layer", e);
+        }
         return (queryResult == null) ? null : mappingService.mapTo(queryResult, ReservationDTO.class);
     }
 
     @Override
     public Collection<ReservationDTO> getByCustomer(Long customerId) {
-        return mappingService.mapTo(service.findByCustomer(customerId), ReservationDTO.class);
+        Collection<Reservation> reservations;
+        try {
+            reservations = service.findByCustomer(customerId);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not obtain Reservations from the persistence layer", e);
+        }
+        return mappingService.mapTo(reservations, ReservationDTO.class);
     }
 
     @Override
     public Collection<ReservationDTO> getByTrip(Long tripId) {
-        return mappingService.mapTo(service.findByTrip(tripId), ReservationDTO.class);
+        Collection<Reservation> reservations;
+        try {
+            reservations = service.findByTrip(tripId);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not obtain Reservations from the persistence layer", e);
+        }
+        return mappingService.mapTo(reservations, ReservationDTO.class);
     }
 
     @Override
@@ -71,7 +95,13 @@ public class ReservationFacadeImpl implements ReservationFacade {
             throw new IllegalArgumentException("Tried to update reservation without ID");
         }
         Reservation reservation = mappingService.mapTo(data, Reservation.class);
-        service.update(reservation);
+        try {
+            service.update(reservation);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not update Reservation in the persistence layer", e);
+        }
+
+
     }
 
     @Override
@@ -83,11 +113,21 @@ public class ReservationFacadeImpl implements ReservationFacade {
             throw new IllegalArgumentException("tried to delete reservation without ID");
         }
         Reservation reservation = mappingService.mapTo(reservationDTO, Reservation.class);
-        service.remove(reservation);
+        try {
+            service.remove(reservation);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not remove Reservation in the persistence layer", e);
+        }
     }
 
     @Override
     public Collection<ReservationDTO> getReservationByInterval(LocalDate from, LocalDate to) {
-        return mappingService.mapTo(service.findReservationsBetween(from, to), ReservationDTO.class);
+        Collection<Reservation> result;
+        try {
+            result = service.findReservationsBetween(from, to);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not obtain Reservations from the persistence layer", e);
+        }
+        return mappingService.mapTo(result, ReservationDTO.class);
     }
 }
