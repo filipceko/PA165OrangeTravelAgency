f4476f9378f0016d073b28ae0d31c6ce0956fb85
lyrithy <lyrithyit@gmail.com>
2018-11-26 13:43:19 +0100

    UpdateTest

--- TravelAgency-service/src/test/java/cz/muni/fi/travelAgency/service/TripServiceTest.java
+++ TravelAgency-service/src/test/java/cz/muni/fi/travelAgency/service/TripServiceTest.java
@@ -1,20 +1,20 @@
 package cz.muni.fi.travelAgency.service;
 
-import cz.muni.fi.travelAgency.config.ServiceConfiguration;
-import cz.muni.fi.travelAgency.dao.TripDao;
-import cz.muni.fi.travelAgency.entities.Trip;
-import org.mockito.*;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+        import cz.muni.fi.travelAgency.config.ServiceConfiguration;
+        import cz.muni.fi.travelAgency.dao.TripDao;
+        import cz.muni.fi.travelAgency.entities.Trip;
+        import org.mockito.*;
+        import org.springframework.test.context.ContextConfiguration;
+        import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+        import org.testng.Assert;
+        import org.testng.annotations.BeforeClass;
+        import org.testng.annotations.BeforeMethod;
+        import org.testng.annotations.Test;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+        import java.time.LocalDate;
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
 
 @ContextConfiguration(classes = ServiceConfiguration.class)
 public class TripServiceTest extends AbstractTestNGSpringContextTests {
@@ -82,54 +82,63 @@ public class TripServiceTest extends AbstractTestNGSpringContextTests {
     }
 
     @Test
+    public void getAllTripTest() {
+        Trip trip1 = new Trip();
+        trip1.setId(11L);
+        trip1.setFromDate(firstDate);
+        trip1.setToDate(secondDate);
+        trip1.setDestination(destination);
+        trip1.setCapacity(5);
+        trip1.setPrice(100.20);
+
+        Trip trip2 = new Trip();
+        trip2.setId(12L);
+        trip2.setFromDate(firstDate);
+        trip2.setToDate(secondDate);
+        trip2.setDestination("Prague");
+        trip2.setCapacity(4);
+        trip2.setPrice(120.20);
+
+        Trip trip3 = new Trip();
+        trip3.setId(13L);
+        trip3.setFromDate(firstDate);
+        trip3.setToDate(secondDate);
+        trip3.setDestination("Paris");
+        trip3.setCapacity(3);
+        trip3.setPrice(320.20);
+
+        List<Trip> allTrips = Arrays.asList(trip1,trip2,trip3);
+        Mockito.when(tripDao.findAll()).thenReturn(allTrips);
+        List<Trip> getTrips = new ArrayList<>(tripService.findAll());
+        Assert.assertEquals(3,getTrips.size());
+        Assert.assertEquals(getTrips.size(), 3);
+        Assert.assertEquals(getTrips.get(0),trip1);
+        Assert.assertEquals(getTrips.get(1),trip2);
+        Assert.assertEquals(getTrips.get(2),trip3);
+    }
+
+    @Test
     public void updateTripTest() {
         Assert.assertEquals(trip.getDestination(),"Lake Island");
-        Mockito.when(tripDao.findById(trip.getId())).thenReturn(trip);
-        Trip findTrip = tripService.findById(trip.getId());
 
-        findTrip.setDestination("New Lake Island");
-        findTrip.setCapacity(15);
-        findTrip.setPrice(150.20);
-        tripService.updateTrip(findTrip);
-        Trip getTripUpdate = tripService.findById(trip.getId());
-        Assert.assertEquals(getTripUpdate.getDestination(),"New Lake Island");
-        Assert.assertEquals(getTripUpdate.getCapacity(),15);
+        trip.setDestination("New Lake Island");
+        trip.setCapacity(15);
+        trip.setPrice(150.20);
+        tripService.updateTrip(trip);
+        Mockito.verify(tripDao, Mockito.times(1)).update(trip);
     }
 
     @Test
     public void removeTripTest(){
         tripService.removeTrip(trip);
-        Mockito.verify(tripDao).remove(trip);
+        Mockito.verify(tripDao, Mockito.times(1)).remove(trip);
     }
 
     @Test
     public  void findByIdTest(){
         Mockito.when(tripDao.findById(trip.getId())).thenReturn(trip);
-        Assert.assertEquals(trip,tripService.findById(trip.getId()));
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void updateNullTest(){
-        tripService.updateTrip(null);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void removeNullTest(){
-        tripService.removeTrip(null);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void createNullTest(){
-        tripService.createTrip(null);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void findDestinationNullTest(){
-        tripService.findByDestination(null);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void findIntervalNullTest(){
-        tripService.findByInterval(null,null);
+        Trip foundTrip = tripService.findById(trip.getId());
+        Assert.assertEquals(foundTrip,trip);
+        Assert.assertEquals(foundTrip.getDestination(),trip.getDestination());
     }
 }
