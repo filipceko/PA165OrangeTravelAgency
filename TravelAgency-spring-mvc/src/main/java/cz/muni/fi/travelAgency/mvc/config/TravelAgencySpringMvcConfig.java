5b8a0b03b381d73700a83cd0e53b728e4296a3ef
Filip Čekovský <filipceko@gmail.com>
2018-12-03 23:56:13 +0100

    Spring MVC Initial setup, Home page created, pageTemplate created

---
+++ TravelAgency-spring-mvc/src/main/java/cz/muni/fi/travelAgency/mvc/config/TravelAgencySpringMvcConfig.java
@@ -0,0 +1,89 @@
+package cz.muni.fi.travelAgency.mvc.config;
+
+import cz.muni.fi.travelAgency.sampleData.SampleDataConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.validation.Validator;
+
+/**
+ * The central Spring context and Spring MVC configuration.
+ * The @Configuration annotation declares it as Spring configuration.
+ * The @EnableWebMvc enables default  MVC config for using @Controller, @RequestMapping and so on,
+ * see http://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html#mvc-config-enable
+ *
+ * @author Filip Cekovsky
+ */
+@EnableWebMvc
+@Configuration
+@Import(SampleDataConfiguration.class)
+public class TravelAgencySpringMvcConfig implements WebMvcConfigurer {
+
+    private final static Logger logger = LoggerFactory.getLogger(TravelAgencySpringMvcConfig.class);
+
+    private static final String TEXTS = "Texts";
+
+    /**
+     * Maps the main page to a specific view.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        logger.debug("mapping URL / to home view");
+        registry.addViewController("/").setViewName("home");
+    }
+
+
+    /**
+     * Enables default Tomcat servlet that serves static files.
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        logger.debug("enabling default servlet for static files");
+        configurer.enable();
+    }
+
+    /**
+     * Provides mapping from view names to JSP pages in WEB-INF/jsp directory.
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        logger.debug("registering JSP in /WEB-INF/jsp/ as views");
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    /**
+     * Provides localized messages.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        logger.debug("registering ResourceBundle 'Texts' for messages");
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(TEXTS);
+        return messageSource;
+    }
+
+    /**
+     * Provides JSR-303 Validator.
+     */
+    @Bean
+    public Validator validator() {
+        logger.debug("registering JSR-303 validator");
+        return new LocalValidatorFactoryBean();
+    }
+
+}
--- TravelAgency-spring-mvc/src/main/webapp/WEB-INF/jsp/home.jsp
+++ TravelAgency-spring-mvc/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -5,12 +5,32 @@
   Time: 17:45
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Orange Travel Agency</title>
-</head>
-<body>
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="utf-8" trimDirectiveWhitespaces="true" session="false" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="my" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
-</body>
-</html>
+<my:pageTemplate>
+    <jsp:attribute name="title">Welcome!</jsp:attribute>
+    <jsp:attribute name="body">
+
+    <div class="jumbotron">
+        <h1>Orange Travel Agency</h1>
+        <p class="lead">The best place you can be!</p>
+    </div>
+
+    <div class="row">
+        <c:forEach begin="1" end="10" var="i">
+            <div class="col-xs-12 col-sm-6 col-md-2 col-lg-1">
+                <p>
+                    <button class="btn btn-default">Button ${i}</button>
+                </p>
+            </div>
+        </c:forEach>
+    </div>
+
+</jsp:attribute>
+</my:pageTemplate>
+
